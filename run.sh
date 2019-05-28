3a5b077d3b6e2bb4fc464783994ac9489d67a933
hemapriyamaheswaran <45891957+hemapriyamaheswaran@users.noreply.github.com>
2019-05-28 14:36:22 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+terraform output kube_config > ~/.kube/aksconfig
+export KUBECONFIG=~/.kube/aksconfig
+kubectl apply -f deployment.yml
+kubectl apply -f https://k8s.io/examples/pods/storage/pv-volume.yaml
+kubectl apply -f https://k8s.io/examples/pods/storage/pv-claim.yaml
