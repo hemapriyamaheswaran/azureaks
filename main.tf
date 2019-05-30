3d656a4a2dc10fbce13941adae3a998460bc54d3
hemapriyamaheswaran <45891957+hemapriyamaheswaran@users.noreply.github.com>
2019-05-30 12:03:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,25 +7,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "aks_demo" {
   name     = "kubedemo"
   location = "centralus"
-}
-resource "tls_private_key" "key" {
-  algorithm   = "RSA"
-}
-
-## Save the private key in the local workspace ##
-resource "null_resource" "save-key" {
-  triggers {
-    key = "${tls_private_key.key.private_key_pem}"
   }
-
-  provisioner "local-exec" {
-    command = <<EOF
-      mkdir -p ${path.module}/.ssh
-      echo "${tls_private_key.key.private_key_pem}" > ${path.module}/.ssh/id_rsa
-      chmod 0600 ${path.module}/.ssh/id_rsa
-EOF
-  }
-}
 ## AKS kubernetes cluster ##
 resource "azurerm_kubernetes_cluster" "aks_demo" {
   name                = "kubecluster"
